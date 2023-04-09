5c512adfe157cbcf6b56f82d531109cf46c2134a
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2023-04-09 14:17:01 +0700

    Modified create post -> create post to find tutor

--- client/src/views/components/Post.js
+++ client/src/views/components/Post.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import '../../public/css/Post.css';
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import draftToHtml from 'draftjs-to-html';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 
 function Post(props) {
-    const blog = props.post
-    let strjson = props.post.content
-    console.log('Type là' + typeof(strjson))
-    // console.log(json)
-    const html = draftToHtml(strjson);
-    console.log(html)
     return (
         <div className={`post-wrapper`}>
             <div className='post-user'>
@@ -30,7 +21,7 @@ function Post(props) {
                 {/* {blog.title} */}
             </div>
             <div className='post-content'>
-                <div dangerouslySetInnerHTML={{ __html: html }} />;
+
             </div>
         </div>
     )
--- client/src/views/pages/CreatePost.js
+++ client/src/views/pages/CreatePost.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import '../../public/css/CreatePost.css'
 import Button from 'react-bootstrap/Button'
 import Navbar from '../components/Navbar';
@@ -10,22 +7,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginSuccess } from '../../redux/authSlice';
 import { createPost } from '../../redux/apiRequest'
-const BlogEditor = () => {
+const PostEditor = () => {
     const user = useSelector((state) => state.auth.login?.currentUser)
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
-    );
-    const [title, setTitle] = useState("Bài viết mới")
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
     let axiosJWT = createAxios(user, dispatch, loginSuccess);
     const handleSave = () => {
-        const contentState = editorState.getCurrentContent();
-        const rawState = convertToRaw(contentState);
         let newpost = {
-            title: title,
-            content: rawState,
+
             time: Date.now(),
             user_id: user._id
         }
@@ -39,17 +29,106 @@ const BlogEditor = () => {
             navigate("/")
         }
     }, [])
+
+    const [selectedPlace, setSelectedPlace] = useState(null);
+
+    const onPlaceSelected = (place) => {
+        setSelectedPlace(place);
+    };
     return (
         <div className="createpost-wrapper">
             <Navbar>
             </Navbar>
-            <h1>Text Editor</h1>
-            <Editor
-                editorState={editorState}
-                onEditorStateChange={setEditorState}
-            />
-            <Button className="createpost-btn" variant="primary" onClick={handleSave}></Button>
-        </div>
+            <h1>Mô tả yêu cầu gia sư cần tìm</h1>
+            <form className='createpost'>
+                <div className="form-item item1">
+                    <label>
+                        Số điện thoại liên hệ
+                    </label>
+                    <input type="text" className="phonenumber" />
+                </div>
+                <div className="form-item item2">
+                    <label>
+                        Tóm tắt yêu cầu
+                    </label>
+                    <input type="text" className="summary" />
+                </div>
+                <div className="form-item item3">
+                    <label>
+                        Địa điểm dạy
+                    </label>
+                    <input type="text" className="location" />
+                </div>
+                <div className="form-item item4">
+                    <label>
+                        Số học viên
+                    </label>
+                    <input type="text" className="student-number" />
+                </div>
+                <div className="form-item item5">
+                    <label>
+                        Ngày bắt đầu
+                    </label>
+                    <input type="date" className="date-start" />
+                </div>
+                <div className="form-item item6">
+                    <label>
+                        Giờ mỗi buổi
+                    </label>
+                    <input type="text" className="time" />
+                </div>
+                <div className="form-item item7">
+                    <label>
+                        Môn học
+                    </label>
+                    <input type="text" className="subject" />
+                </div>
+                <div className="form-item item8"><label>
+                    Gender
+                </label>
+                    <input type="text" className="gender" /></div>
+                <div className="form-item item9">
+                    <h1>Yêu cầu gia sư</h1>
+                </div>
+                <div className="form-item item10">
+                    <label>
+                        Giới tính gia sư
+                    </label>
+                    <input type="text" className="tutor-gender" />
+                </div>
+                <div className="form-item item11">
+                    <label>
+                        Trình độ
+                    </label>
+                    <input type="text" className="level" />
+                </div>
+                <div className="form-item item12">
+                    <label>
+                        Học phí (vnđ)
+                    </label>
+                    <input type="text" className="salary" />
+                </div>
+                <div className="form-item item13">
+                    <label>
+                        Trả theo
+                    </label>
+                    <input type="text" className="pay-time" />
+                </div>
+                <div className="form-item item14">
+                    <label>
+                        Số buổi/tuần
+                    </label>
+                    <input type="text" className="class-sessions" />
+                </div>
+                <div className="form-item item15">
+                    <label>
+                        Mô tả khác
+                    </label>
+                    <input type="text" className="differ-summary" />
+                </div>
+            </form>
+            <Button className="createpost-btn" variant="primary" onClick={handleSave}>Tạo tìm kiếm</Button>
+        </div >
     )
 }
-export default BlogEditor;
+export default PostEditor;
